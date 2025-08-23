45bb2e1036279937493709d8a693db77233e4149
higor <higorbandeira@hotmail.com>
2025-08-23 17:48:45 -0300

    feat: Add Lib to encapsulate OpenAPI Moderator

---
+++ Moderador.Lib/Models/ModerarTextoRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moderador.Lib.Models
+{
+    public class ModerarTextoRequest
+    {
+        public string Texto { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
---
+++ Moderador.Lib/Services/OpenAI_APIService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ClientModel;
+using Moderador.Lib.interfaces;
+using Moderador.Lib.Models;
+using OpenAI.Moderations;
+
+namespace Moderador.Lib.Services
+{
+    public class OpenAI_APIService : IOpenAI_API
+    {
+        public Task<ClientResult<ModerationResult>> ModerarTexto(ModerarTextoRequest moderarTextoRequest)
+        {
+            var client = OpenAIClientFactory.CreateModerationClientFactory(moderarTextoRequest.ApiKey);
+
+            return client.ClassifyTextAsync(moderarTextoRequest.Texto);
+        }
+    }
+}
